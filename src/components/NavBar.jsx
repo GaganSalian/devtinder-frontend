dd26fbe17c4d3c80869b4f02cc8b0b303694efc8
Your Name <you@example.com>
2025-03-02 10:43:38 +0530

    redirecting to login page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
@@ -8,7 +9,7 @@ const NavBar = () => {
     
         <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder</a>
+    <Link  to="/" className="btn btn-ghost text-xl">DevTinder</Link>
      </div>
       <div className="flex gap-2">
       {user &&(<div className="dropdown dropdown-end mx-5 flex ">
@@ -24,10 +25,10 @@ const NavBar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
