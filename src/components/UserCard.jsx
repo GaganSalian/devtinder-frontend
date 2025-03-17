55a37b0475302732db97f35dba32c6b5d1e929b6
Your Name <you@example.com>
2025-03-17 11:34:24 +0530

    update to aws

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,26 +4,36 @@ import { useDispatch } from "react-redux";
 import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user }) => {
+   // Moved useDispatch outside the render block
+  
+  // console.log("UserCard received user:", user); // Debugging User Data
+
+  // if (!user) {
+  //   console.log("User is undefined, skipping rendering...");
+  //   return null;
+  // }
+
   const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
   const dispatch = useDispatch();
-
   const handleSendRequest = async (status, userId) => {
     try {
+      console.log(`Sending ${status} request for user:`, userId); // Debug log
       const res = await axios.post(
-        BASE_URL + "/request/send/" + status + "/" + userId,
+        `${BASE_URL}/request/send/${status}/${userId}`,
         {},
         { withCredentials: true }
       );
+      console.log("Response from server:", res.data);
       dispatch(removeUserFromFeed(userId));
     } catch (err) {
-      console.err("remove user not working",err)
+      console.error("Error removing user:", err);
     }
   };
-
+//"https://via.placeholder.com/150"
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure>
-        <img src={user.photoUrl} alt="photo" />
+        <img src={user.photoUrl } alt="photo" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
@@ -47,4 +57,5 @@ const UserCard = ({ user }) => {
     </div>
   );
 };
+
 export default UserCard;
